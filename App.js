9992284f277b72694c4e2028e1c3d2fd256de05c
Liyakat Shaikh <liyakatshaikh07@gmail.com>
2021-03-14 13:35:27 +0530

    header done

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import CarItem from './components/CarItem'
+//import CarItem from './components/CarItem'
+import CarList from './components/CarsList'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
-    <CarItem />
+    <Header />
+    <CarList />
 
 
     
@@ -18,7 +20,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    //flex: 1,
+    flex: 1,
+    zIndex:0
     //backgroundColor: '#fff',
     //alignItems: 'center',
     //justifyContent: 'center'
--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
 
 const styles = StyleSheet.create({
     carContainer:{
         width: '100%',
-        height: '100%'
+        height: Dimensions.get('window').height
       },
       titles:{
         marginTop: '30%',
@@ -25,6 +25,15 @@ const styles = StyleSheet.create({
         resizeMode: 'cover',
         position: 'absolute'
     
+      },
+      buttonsContainer:{
+        position: 'absolute',
+        bottom: 50,
+        width: '100%'
+      },
+      subtitleCTA:{
+        textDecorationLine: 'underline',
+        
       }
 });
 
