e1358cca64e7fe2128bc55fe7ad4c123e635ac48
Liyakat Shaikh <liyakatshaikh07@gmail.com>
2021-03-14 11:16:25 +0530

    btn component done

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+    <CarItem />
+
+
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +18,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    //flex: 1,
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center'
+  }
+  
 });
